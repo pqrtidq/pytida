db3ca4a8dfc987869c03b6730ab7efacf87698b4
pqrtidq <jacob.partida@humboldt.edu>
2022-12-14 09:15:15 -0800

    Update sst_attempt.py

--- sst_attempt.py
+++ sst_attempt.py
@@ -1,1 +1,29 @@
-#!/usr/bin/env python3# -*- coding: utf-8 -*-"""Learning python: Plotting downloaded SST near Trinidad@author: pqrtidq"""# %% Importfrom erddapy import erddapy # %% Construct ERDDAP urle = erddapy.ERDDAP(server="UAF",protocol="griddap")e.dataset_id = "nasa_jpl_dde5_3be1_897b"e.griddap_initialize()e.constraints["latitude>="] = 38e.constraints["latitude<="] = 46e.constraints["longitude>="] = -130e.constraints["longitude<="] = -123# %% Downloadds = e.to_xarray()ds["analysed_sst"].plot()ds
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Learning python: Plotting downloaded SST near Trinidad
+
+@author: pqrtidq
+"""
+
+# %% Import
+
+from erddapy import erddapy
+
+
+# %% Construct ERDDAP url
+
+e = erddapy.ERDDAP(server="UAF", protocol="griddap")
+e.dataset_id = "nasa_jpl_dde5_3be1_897b"
+e.griddap_initialize()
+e.constraints["latitude>="] = 38
+e.constraints["latitude<="] = 46
+e.constraints["longitude>="] = -130
+e.constraints["longitude<="] = -123
+
+
+# %% Download
+
+ds = e.to_xarray()
+ds["analysed_sst"].plot()
+ds
